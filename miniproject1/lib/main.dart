cdd4ebe481f6c59d80bfef127a254634785dec2d
yeabsera <yeab.dev@gmail.com>
2024-10-26 13:17:38 +0300

    style(ui): update layout and colors

--- miniproject1/lib/main.dart
+++ miniproject1/lib/main.dart
@@ -12,37 +12,53 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
-  bool top = false;
-
+  Alignment alignment = Alignment.topCenter;
+  Color containerColor = Colors.orange;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Text(
+                  "Welcome",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.yellow[800],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
               SizedBox(
-                height: 400,
+                height: MediaQuery.sizeOf(context).height * 0.5,
                 width: double.infinity,
                 child: AnimatedAlign(
-                  alignment: top ? Alignment.bottomCenter : Alignment.topCenter,
+                  alignment: alignment,
                   duration: const Duration(seconds: 1),
-                  curve: top ? Curves.bounceOut : Curves.bounceIn,
-                  child: Container(
+                  curve: Curves.bounceOut,
+                  child: AnimatedContainer(
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.bounceInOut,
                     width: 100,
                     height: 100,
-                    decoration: const BoxDecoration(
+                    decoration: BoxDecoration(
                       shape: BoxShape.circle,
-                      color: Colors.green,
+                      color: containerColor,
                     ),
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 100,
+              ),
               ElevatedButton(
                 onPressed: () {
                   setState(() {
-                    top = !top;
+                    alignment = Alignment.bottomCenter;
+                    containerColor = Colors.green;
                   });
                 },
                 child: const Text("Start"),
